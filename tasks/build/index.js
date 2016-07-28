9b4c8e66177d08bc90374b55a769f0fffd298ec3
alexbowen <alexbowen123@googlemail.com>
2016-07-28 10:21:42 +0100

    add transpiling of es6 output to assets folder

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
+var gulp = require('gulp');
+
 module.exports = function () {
     require('./sass/compile')();
     require('./mustache/compile')();
+    require('./javascript/bundle')();
+
+    gulp.task('build', ['bundle-scripts', 'compile-sass']);
 };
